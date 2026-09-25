cc6c2824d22be53849bf0b0e1da8535a255c9fa6
SAADRAFIQ278 <mrk544939@gmail.com>
2026-09-24 21:30:17 -0700

    Code by Saad

---
+++ Assignment_01.py
@@ -0,0 +1,66 @@
+# name = input("Enter your name: ")
+# color = input("Enter your favorite color: ")
+
+# print("Hello", name + "!", "Your favorite color is", color + ".")
+
+# age = int(input("Enter your age in years: "))
+# months = age * 12
+# print("You are approximately", months, "months old!")
+
+# hours = float(input("Enter hours worked: "))
+# rate = float(input("Enter hourly pay rate: "))
+# earnings = hours * rate
+# print("Your earning per week is approximately ", earnings)
+
+# length = float(input("Enter length: "))
+# width = float(input("Enter width: "))
+
+# area = length * width
+# perimeter = 2 * (length + width)
+# print(area)
+# print(perimeter)
+
+# candies = 21
+# children = 4
+# full_candies = candies // children
+# leftover = candies % children
+# print(full_candies)
+# print(leftover)
+
+# age = int(input("Enter your age: "))
+# print(age >= 18)
+
+# score = int(input("Enter your exam score: "))
+# print(score >= 50)
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+
+# print(num1 == num2)
+
+# number = int(input("Enter a number: "))
+# print(number % 2 == 0)
+
+# age = int(input("Enter your age: "))
+# print(age >= 13 and age <= 19)
+
+# colors = ["Red", "Green", "Blue"]
+
+# print(colors[0])
+# print(colors[2])
+
+# foods = ["Pizza", "Burger", "Pasta"]
+# foods[1] = "Sandwich"
+# print(foods)
+
+# items = ["Pen", "Notebook"]
+# new_item = input("Enter another school item: ")
+# items.append(new_item)
+# print(items)
+
+# numbers = [10, 20, 30, 40]
+# print(len(numbers) > 3)
+
+# fruits = ["Apple", "Banana", "Cherry", "Date"]
+# fruits[0], fruits[-1] = fruits[-1], fruits[0]
+# print(fruits)
