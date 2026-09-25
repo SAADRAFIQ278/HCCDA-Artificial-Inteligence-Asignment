8790b99f6b4269843b64b617e611c6668d0299f1
SAADRAFIQ278 <mrk544939@gmail.com>
2026-09-24 21:40:53 -0700

    Edit by Saad

---
+++ Assignment_02.py
@@ -0,0 +1,69 @@
+# value = 10
+# print(value, type(value))
+
+# value = "Hello"
+# print(value, type(value))
+
+# value = 10.5
+# print(value, type(value))
+
+# value = True
+# print(value, type(value))
+
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+
+# print("Addition:", num1 + num2)
+# print("Subtraction:", num1 - num2)
+# print("Multiplication:", num1 * num2)
+# print("Division:", num1 / num2)
+
+# number = int(input("Enter a number: "))
+
+# if number % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+# marks = int(input("Enter marks: "))
+
+# if marks >= 80:
+#     print("A")
+# elif marks >= 70:
+#     print("B")
+# elif marks >= 60:
+#     print("C")
+# elif marks >= 50:
+#     print("D")
+# else:
+#     print("Fail!")
+
+# for number in range(1, 21):
+#     if number % 2 == 0:
+#         print(number)
+
+# fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"]
+
+# for fruit in fruits:
+#     print(fruit)
+
+# def is_even(number):
+#     if number % 2 == 0:
+#         return True
+#     else:
+#         return False
+
+# print(is_even(10))
+# print(is_even(7))
+
+# subjects = []
+
+# number = int(input("How many subjects? "))
+
+# for i in range(number):
+#     subject = input("Enter subject name: ")
+#     marks = float(input("Enter marks: "))
+#     subjects.append([subject, marks])
+
+# for subject in subjects:
+#     print(subject)
